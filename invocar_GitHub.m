69efe1a10d331bdc85218ad0fd5e5a208867c994
Amanda Beuthner <beuthner1@gmail.com>
2025-02-24 01:33:11 -0300

    trazer um código que deseja do GITHUB

---
+++ invocar_GitHub.m
@@ -0,0 +1,10 @@
+let
+  //trocar o link para buscar o seu GIT, com o repositório e a fórmula 
+
+  Git = Web.Contents("https://raw.githubusercontent.com/Abeuthner/codigos_M/refs/heads/main/fx_soma_M.txt")
+
+
+in
+  
+Git
+
