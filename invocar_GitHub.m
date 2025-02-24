ca0feeae236f92a3c93a26e6cf002b72861f3082
Amanda Beuthner <beuthner1@gmail.com>
2025-02-24 01:44:51 -0300

    completando a função, para que possa executar)

--- invocar_GitHub.m
+++ invocar_GitHub.m
@@ -1,10 +1,18 @@
 let
   //trocar o link para buscar o seu GIT, com o repositório e a fórmula 
 
-  Git = Web.Contents("https://raw.githubusercontent.com/Abeuthner/codigos_M/refs/heads/main/fx_soma_M.txt")
+  Git = Web.Contents("https://raw.githubusercontent.com/Abeuthner/codigos_M/refs/heads/main/fx_soma_M.txt"),
+
+  //lendo o arquivo binário
+
+  script = Text.FromBinary(Git),
+
+  //executando
+
+  run = Expression.Evaluate(script, #shared)
 
 
 in
   
-Git
+run
 
